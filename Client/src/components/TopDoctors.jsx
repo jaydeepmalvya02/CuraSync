7867a531d1b8daa33b29c633eb80943105340888
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-05-27 17:20:24 +0530

    complete till  3:03:00 topdoctor component ui and  All doctors component ui abnd appointment page ui

--- Client/src/components/TopDoctors.jsx
+++ Client/src/components/TopDoctors.jsx
@@ -14,7 +14,7 @@ const TopDoctors = () => {
       </p>
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
         {doctors.slice(0,10).map((item,index)=>(
-          <div 
+          <div key={index}
           onClick={()=>navigate(`/appointment/${item._id}`)}
           className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
             <img  className='bg-blue-50' src={item.image} alt="" />
